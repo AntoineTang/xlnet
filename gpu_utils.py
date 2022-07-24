469b502bf4314570e0beb57d0f8aad02d6efda71
AntoineTang <65993383+AntoineTang@users.noreply.github.com>
2022-07-24 16:03:11 +0800

    Update gpu_utils.py

--- gpu_utils.py
+++ gpu_utils.py
@@ -3,7 +3,8 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+#import tensorflow as tf
 
 def assign_to_gpu(gpu=0, ps_dev="/device:CPU:0"):
     def _assign(op):
