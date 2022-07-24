d572bda81d3054279d3daed9a44f2b66051c47d6
AntoineTang <65993383+AntoineTang@users.noreply.github.com>
2022-07-24 15:53:32 +0800

    Update run_classifier.py

--- run_classifier.py
+++ run_classifier.py
@@ -17,8 +17,8 @@ from copy import copy
 from collections import defaultdict as dd
 
 import absl.logging as _logging  # pylint: disable=unused-import
-import tensorflow as tf
-
+# import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import sentencepiece as spm
 
 from data_utils import SEP_ID, VOCAB_SIZE, CLS_ID
