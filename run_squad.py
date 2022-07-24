a8f30a83280cc223760c34c9d865a1ab68b205d9
AntoineTang <65993383+AntoineTang@users.noreply.github.com>
2022-07-24 16:05:10 +0800

    Update run_squad.py

--- run_squad.py
+++ run_squad.py
@@ -22,7 +22,8 @@ if six.PY2:
 else:
   import pickle
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+#import tensorflow as tf
 import sentencepiece as spm
 from prepro_utils import preprocess_text, encode_ids, encode_pieces, printable_text
 import function_builder
